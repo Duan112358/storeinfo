c270b8c3ca22e45d89f17bbc413cba73eb2e6d1e
duan112358 <duan112358@gmail.com>
2014-04-12 19:48:35 +0800

    bug fixed
    fixed bug caused by css class renaming

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function addComment() {
     var comment_input = document.querySelector(".comment-window input.comment-content");
-    var ratting_ctrl = document.querySelector(".comment-window .ratting");
+    var ratting_ctrl = document.querySelector(".comment-window .rating");
 
     comment_input.addEventListener("focus", function(event) {
     	ratting_ctrl.style.display = "block";
